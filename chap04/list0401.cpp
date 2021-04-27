28d8ff11188817373f2ca29a50a47a13b3cefd22
chc1129 <chc1129@gmail.com>
2021-04-28 07:21:11 +0900

    cpp prg fix

--- chap04/list0401.cpp
+++ chap04/list0401.cpp
@@ -13,3 +13,11 @@ int main()
   cout << "signed char   :" << CHAR_MIN << "～" << CHAR_MAX  << '\n';
   cout << "unsigned char :" << 0        << "～" << UCHAR_MAX << '\n';
 
+  cout << "short int : " << SHRT_MIN << "～" << SHRT_MAX << '\n';
+  cout << "int       : " << INT_MIN  << "～" << INT_MAX  << '\n';
+  cout << "long int  : " << LONG_MIN << "～" << LONG_MAX << '\n';
+
+  cout << "unsigned short int : " << 0 << "～" << USHRT_MAX << '\n';
+  cout << "unsigned int       : " << 0 << "～" << UINT_MAX  << '\n';
+  cout << "unsigned long int  : " << 0 << "～" << ULONG_MAX << '\n';
+}
