b515e0c44b039cc46acc2cbed07728138b5880cb
chc1129 <chc1129@gmail.com>
2021-05-01 04:57:47 +0900

    cpp prg fix

---
+++ chap07/list070c01.cpp
@@ -0,0 +1,12 @@
+// int型の大きさとint*型の大きさを表示
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "sizeof(int)  = " << sizeof(int)  << '\n';
+  cout << "sizeof(int*) = " << sizeof(int*) << '\n';
+
+}
