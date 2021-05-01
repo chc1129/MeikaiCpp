12f345845136f05149bb2ba56897be1402480cf3
chc1129 <chc1129@gmail.com>
2021-05-01 16:49:53 +0900

    cpp prg fix

---
+++ chap08/list0809.cpp
@@ -0,0 +1,17 @@
+// 配列による文字列の書きかえ
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char s[] = "ABC";
+
+  cout << "s = \"" << s << "\"\n";
+
+  s = "XYZ";  // エラー
+
+  cout << "s = \"" << s << "\"\n";
+}
+
